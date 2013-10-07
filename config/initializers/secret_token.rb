40a66c2547bdf3e0008f78424d292c91c852680e
dbc-apprentice <apprentice@dbc-chicago-008.local>
2013-10-07 15:51:33 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CraigsList::Application.config.secret_key_base = '80e9600418c175b9b9d93cffff045d89248a1cb1f3afaa2f2d129cc4c35d0b2b096edccf42a9a0cfaa420aa88b794f997dcb289b2e0f5e7788dc350282531008'
